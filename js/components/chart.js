62f09caa408d02c02202a4c38918333365110079
Alexandr Kazachenko <shoom3301@gmail.com>
2016-06-09 19:20:37 +0600

    Render axis of chart

--- js/components/chart.js
+++ js/components/chart.js
@@ -3,14 +3,36 @@
  */
 class Chart {
     constructor() {
+        this.offsetX = 50;
+        this.offsetY = 40;
+    }
 
+    /**
+     * Coordinate of point by X axis
+     * @param x {number}
+     * @returns {number}
+     */
+    axisX(x){
+        let max = this.x[this.x.length-1][0];
+        return (this.width*x/max)+this.offsetX;
+    }
+    /**
+     * Coordinate of point by Y axis
+     * @param y {number}
+     * @returns {number}
+     */
+    axisY(y){
+        let max = this.y[this.y.length-1][0];
+        return (this.height*y/max)-this.offsetY;
     }
 }
 
 module.exports = {
-    templateUrl: 'js/components/chart.svg',
+    templateUrl: 'js/components/chart.html',
     controller: Chart,
     bindings: {
+        width: '<',
+        height: '<',
         x: '<',
         y: '<',
         data: '<'
